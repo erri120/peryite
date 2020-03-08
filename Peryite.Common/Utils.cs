7643f9ed3ff1751c49a3e6053acaad311543142d
erri120 <erri120@protonmail.com>
2020-03-08 12:46:06 +0100

    Created Utils class

---
+++ Peryite.Common/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Peryite.Common
+{
+    public static class Utils
+    {
+        /// <summary>
+        ///     Executes the action for every item in coll
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="coll"></param>
+        /// <param name="f"></param>
+        public static void Do<T>(this IEnumerable<T> coll, Action<T> f)
+        {
+            foreach (var i in coll) f(i);
+        }
+    }
+}
